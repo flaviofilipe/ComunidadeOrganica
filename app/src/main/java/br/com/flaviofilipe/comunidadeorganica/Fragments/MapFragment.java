ff184653baa1bfc04001b34eeb9abece54d9ef67
flavio filipe <flaviofilipe97@gmail.com>
2018-07-31 01:47:46 -0300

    create firebase project/set cam zoom in my location

--- app/src/main/java/br/com/flaviofilipe/comunidadeorganica/Fragments/MapFragment.java
+++ app/src/main/java/br/com/flaviofilipe/comunidadeorganica/Fragments/MapFragment.java
@@ -8,6 +8,8 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -31,11 +33,15 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import br.com.flaviofilipe.comunidadeorganica.Activitys.MainActivity;
 import br.com.flaviofilipe.comunidadeorganica.R;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback {
+public class MapFragment extends Fragment implements OnMapReadyCallback, LocationListener {
 
     GoogleMap map;
     private LocationManager locationManager;
+    Location myLocation;
     View view;
+    double latitude;
+    double longitude;
+    LatLng locationNow;
 
     //Tela do fragment 1
     @Nullable
@@ -43,6 +49,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragments_map, container, false);
         //requestPermission();
+        latitude = 0.0;
+        longitude = 0.0;
         return view;
     }
 
@@ -53,63 +61,86 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_fragment);
         mapFragment.getMapAsync(this);
 
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
         //ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d("STATUS", "onResume");
+        //Ativa GPS
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
+        } catch (SecurityException ex) {
+            Toast.makeText(getContext(), "Erro permission failed: " + ex, Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            Log.e("LOCATION MANAGER", e.getMessage());
+        }
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //Remove atualização do privider para enconomizar recursos
+        locationManager.removeUpdates(this);
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
         try {
-            //Capturando localização atual
-            locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-            Criteria criteria = new Criteria();
-            String privider = locationManager.getBestProvider(criteria, true);
-            Toast.makeText(getActivity(), "Provider: "+privider, Toast.LENGTH_SHORT);
 
             map = googleMap;
 
             //Add btn zoom
             map.getUiSettings().setZoomControlsEnabled(true);
             map.setMyLocationEnabled(true);
+            map.setMinZoomPreference(15);
         } catch (SecurityException ex) {
             Log.e("Erro Location Map", "ERRO", ex);
         }
 
-        LatLng ifba = new LatLng(-14.841926, -40.8793097);
+        LatLng ifba = new LatLng(-14.841753, -40.877857);
         MarkerOptions marker = new MarkerOptions();
         marker.position(ifba).title("IFBA");
 
         map.addMarker(marker);
-        map.moveCamera(CameraUpdateFactory.newLatLng(ifba));
 
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        //Quando muda a localização
 
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        locationNow = new LatLng(latitude, longitude);
+        map.moveCamera(CameraUpdateFactory.newLatLng(locationNow));
+    }
 
-    /*
-    Ativar GPS
-    private void createNoGpsDialog(){
-        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                switch (which) {
-                    case DialogInterface.BUTTON_POSITIVE:
-                        Intent callGPSSettingIntent = new Intent(
-                                android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                        startActivity(callGPSSettingIntent);
-                        break;
-                }
-            }
-        };
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        AlertDialog mNoGpsDialog = builder.setMessage("Por favor ative seu GPS para usar esse aplicativo.")
-                .setPositiveButton("Ativar", dialogClickListener)
-                .create();
-        mNoGpsDialog.show();
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        //Muda o status
 
     }
-    */
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        //Provider habilitado
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        //Provider Desabilitado
+    }
+
 
 }
 
